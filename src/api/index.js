4911348ff7ab6115da6a8b1b251aaf16248f1ab9
Tanuj Bordikar <91179998+tanujbordikar@users.noreply.github.com>
2023-07-12 13:02:09 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://travelogue.adaptable.app/' });
+const API = axios.create({ baseURL: 'https://travelogue-server.onrender.com' });
 API.interceptors.request.use((req) => {
    if(localStorage.getItem('profile')) {
       req.headers.authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
